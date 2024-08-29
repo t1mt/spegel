606e436967f890f46180bbcc2f3efa1ad001a2b4
Tim <t1mt>
2024-08-29 15:13:50 +0800

    refactor setup logger with zap and ipfs logger

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -134,6 +134,7 @@ func (r *Registry) handle(rw mux.ResponseWriter, req *http.Request) {
 			"status", rw.Status(),
 			"method", req.Method,
 			"latency", latency.String(),
+			"length", rw.Header().Get("Content-Length"),
 			"ip", getClientIP(req),
 			"handler", handler,
 		}
